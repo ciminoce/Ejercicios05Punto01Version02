65f1cee00e7f7e651b66d16726fd9060063cae71
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-29 18:50:46 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicios05Punto01Version02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicios05Punto01Version02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Segundo Ejemplo de Ciclos";
+            var suma = 0;
+            var contador = 0;
+            do
+            {
+                Console.Write($"Ingrese el número {contador + 1}:");
+                var nro = int.Parse(Console.ReadLine());
+                suma += nro;
+                contador++;
+            } while (contador < 10);
+
+            Console.Write($"La suma de {contador} números es {suma}:");
+
+
+            
+		}
+    }
+}
